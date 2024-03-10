a4b223f7fb359726d561c65adbaf18176e689944
Matt Greer <matt.e.greer@gmail.com>
2024-03-10 17:32:42 -0400

    README: add status

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # King of Fighters 94: Team Edit Edition
 
 A ROM hack for KOF94 that adds team edit. Essentially it ports the character select screen from kof95 over.
+
+## Status
+
+Just started working on this. It's going to be (quite) a while.
--- src/lua/forceTeamOfTriplets.lua
+++ src/lua/forceTeamOfTriplets.lua
@@ -1,15 +1,16 @@
 cpu = manager.machine.devices[":maincpu"]
 mem = cpu.spaces["program"]
 
-local char1_address = 0x1081c1
-local char2_address = 0x1081c2
+local p1_char1_address = 0x1081c1
+local p1_char2_address = 0x1081c2
+local p2_char1_address = 0x1083c1
+local p2_char2_address = 0x1083c2
 
-function on_memory_write(offset, data)
-	local CHARACTER_ID = 0x1b
+CHARACTER_ID = 0x17
 
+function on_p1_memory_write(offset, data)
 	local tag = mem:read_range(0x108110, 0x108117, 8)
 
-	print(string.format("tag: %s, offset: %x - %x", tag, offset, data))
 	if tag == "P1MEMBER" and offset == 0x1081c0 then
 		return CHARACTER_ID << 8 | CHARACTER_ID
 	end
@@ -19,4 +20,17 @@ function on_memory_write(offset, data)
 	end
 end
 
-mem_handler = mem:install_write_tap(char1_address - 1, char2_address + 1, "writes", on_memory_write)
+function on_p2_memory_write(offset, data)
+	local tag = mem:read_range(0x108310, 0x108317, 8)
+
+	if tag == "P2MEMBER" and offset == 0x1083c0 then
+		return CHARACTER_ID << 8 | CHARACTER_ID
+	end
+
+	if tag == "P2MEMBER" and offset == 0x1083c2 then
+		return CHARACTER_ID << 8 | CHARACTER_ID
+	end
+end
+
+p1_mem_handler = mem:install_write_tap(p1_char1_address - 1, p1_char2_address + 1, "writes", on_p1_memory_write)
+p2_mem_handler = mem:install_write_tap(p2_char1_address - 1, p2_char2_address + 1, "writes", on_p2_memory_write)
