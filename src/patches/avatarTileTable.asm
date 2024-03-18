3719dae358dbf6aadd06a3d656e5ccaaceed0ba7
Matt Greer <matt.e.greer@gmail.com>
2024-03-18 19:40:14 -0400

    avatar tile table

---
+++ src/patches/avatarTileTable.asm
@@ -0,0 +1,121 @@
+;;; the offset into the character grid of the character's four tiles
+;;; 0: Heidern
+dc.b 12
+dc.b 13
+dc.b 30
+dc.b 31
+;;; 1: Ralf
+dc.b 14
+dc.b 15
+dc.b 32
+dc.b 33
+;;; 2: Clark
+dc.b 16
+dc.b 17
+dc.b 34
+dc.b 35
+;;; 3: Athena
+dc.b 36
+dc.b 37
+dc.b 54
+dc.b 55
+;;; 4: Kensou
+dc.b 38
+dc.b 39
+dc.b 56
+dc.b 57
+;;; 5: Chin
+dc.b 40
+dc.b 41
+dc.b 58
+dc.b 59
+;;; 6: Kyo
+dc.b 72
+dc.b 73
+dc.b 90
+dc.b 91
+;;; 7: Benimaru
+dc.b 74
+dc.b 75
+dc.b 92
+dc.b 93
+;;; 8: Goro
+dc.b 76
+dc.b 77
+dc.b 94
+dc.b 95
+;;; 9: Heavy
+dc.b 6
+dc.b 7
+dc.b 24
+dc.b 25
+;;; a: Lucky
+dc.b 8
+dc.b 9
+dc.b 26
+dc.b 27
+;;; b: Brian
+dc.b 10
+dc.b 11
+dc.b 28
+dc.b 29
+;;; c: Kim
+dc.b 42
+dc.b 43
+dc.b 60
+dc.b 61
+;;; d: Chang
+dc.b 44
+dc.b 45
+dc.b 62
+dc.b 63
+;;; e: Choi
+dc.b 46
+dc.b 47
+dc.b 64
+dc.b 65
+;;; f: Terry
+dc.b 0
+dc.b 1
+dc.b 18
+dc.b 19
+;;; 10: Andy
+dc.b 2
+dc.b 3
+dc.b 20
+dc.b 21
+;;; 11: Joe
+dc.b 4
+dc.b 5
+dc.b 22
+dc.b 23
+;;; 12: Ryo
+dc.b 78
+dc.b 79
+dc.b 96
+dc.b 97
+;;; 13: Robert
+dc.b 80
+dc.b 81
+dc.b 98
+dc.b 99
+;;; 14: Takuma
+dc.b 82
+dc.b 83
+dc.b 100
+dc.b 101
+;;; 15: Yuri
+dc.b 48
+dc.b 49
+dc.b 66
+dc.b 67
+;;; 16: Mai
+dc.b 50
+dc.b 51
+dc.b 68
+dc.b 69
+;;; 17: King
+dc.b 52
+dc.b 53
+dc.b 70
+dc.b 71
