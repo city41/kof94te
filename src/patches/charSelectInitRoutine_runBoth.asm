7a5b0325b8f802b17d0cbe76a671955b4f036153
Matt Greer <matt.e.greer@gmail.com>
2024-03-18 12:00:38 -0400

    have both approaches at once

--- src/patches/charSelectInitRoutine_runBoth.asm
+++ src/patches/charSelectInitRoutine_runBoth.asm
@@ -1,33 +1,35 @@
 ; originally 36e66 called these routines to set things up
 ; so far, just blindly calling them too
-jsr $3604
-jsr $15a0
-jsr $25aa
-jsr $5e72
-; this is the bios routine for clearing all sprites
-jsr $c004c8
+; jsr $3604
+; jsr $15a0
+; jsr $25aa
+; jsr $5e72
+; ; this is the bios routine for clearing all sprites
+; jsr $c004c8
 
 ; load the palettes
 ; TODO: this should be done during vblank
 jsr $2LOAD_P_A_L_E_T_T_E_S
 ; load the background/oceans image onto the screen
 ; TODO: make sure this is done during vblank
-move.w #321, D6 ; set sprite index to 321
+move.w #326, D6 ; set sprite index to 321
 lea $2BG_OCEANS_IMAGE, A6 ; load the image pointer
 jsr $2RENDER_STATIC_IMAGE
 
 ; load the logo/countries image onto the screen
-move.w #341, D6 ; set sprite index to 21
+move.w #346, D6 ; set sprite index to 21
 lea $2LOGO_COUNTRIES_IMAGE, A6 ; load the image pointer
 jsr $2RENDER_STATIC_IMAGE
 
 ; load the character grid image onto the screen
-move.w #356, D6 ; set sprite index to 21
+move.w #361, D6 ; set sprite index to 21
 lea $2CHARACTER_GRID_IMAGE, A6 ; load the image pointer
 jsr $2RENDER_STATIC_IMAGE
 
 ; now init is done, go into main char select routine
 ; if the timer has elapsed
-move.l #$2CHAR_SELECT_MAIN_ROUTINE, $108584
+; move.l #$2CHAR_SELECT_MAIN_ROUTINE, $108584
+
+move.b #1, $IN_CHAR_SELECT_FLAG
 
 rts
---
+++ src/patches/charSelectMainRoutine_runBoth.asm
@@ -0,0 +1,25 @@
+;;;; NOTE: using A5 needs to be done carefully, the main game
+;;;; expects it when we rts
+move.b #$f, $108235 ; p1 character one is Terry
+move.b #$6, $108236 ; p1 character two is Kyo
+move.b #$12, $108237 ; p1 character three is Ryo
+move.b #$f, $108435 ; p2 character one is Terry
+move.b #$f, $108436 ; p2 character two is Terry
+move.b #$f, $108437 ; p2 character three is Terry
+
+
+move.b $10fdac, D0 ; load BIOS_STATCURNT
+btst #$0, D0 ; is p1 start pressed?
+beq keepTimerGoing ; it's not? let's try again next frame
+
+; move.l #$37eb2, $108584 ; it is, have the engine go to order select next
+; it is, set the countdown to zero so the game will move onto order select on its own
+move.w #0, $108654
+bra done
+
+keepTimerGoing:
+; set the timer back to 1400 so we stay on team select indefinitely
+move.w #$1400, $108654
+
+done:
+rts
