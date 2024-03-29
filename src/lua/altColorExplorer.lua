373760a3cf04fa550851bbca43087d0cd5627a5c
Matt Greer <matt.e.greer@gmail.com>
2024-03-29 13:05:28 -0400

    altColorExplorer: forces team 1 to alt colors

--- src/lua/altColorExplorer.lua
+++ src/lua/altColorExplorer.lua
@@ -1,15 +1,18 @@
 cpu = manager.machine.devices[":maincpu"]
 mem = cpu.spaces["program"]
 
-address = 0x1081d8
-ret = 0x0000
+min_address = 0x1029d8
+max_address = 0x102dd8
+addresses = { [min_address] = true, [0x102bd8] = true, [max_address] = true }
+ret = 0x0100
 
 function on_p1_memory_read(offset, data, mask)
 	local tag = mem:read_range(0x108110, 0x108117, 8)
+	print(string.format("tag: %s, offset: %x, data: %x, mask: %x", tag, offset, data, mask))
 
-	if tag == "P1MEMBER" and offset == address then
-		print(string.format("%x:forcing %x over %x, mask: %x", address, ret, data, mask))
+	if (tag == "P1MEMBER" or tag == "P1  TEAM") and addresses[offset] then
+		return ret
 	end
 end
 
-p1_mem_handler = mem:install_read_tap(address, address + 3, "reads", on_p1_memory_read)
+p1_mem_handler = mem:install_read_tap(min_address, max_address + 3, "reads", on_p1_memory_read)
