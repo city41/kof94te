7ea61462afe468843b65a429eb8d7b274ac6c930
Matt Greer <matt.e.greer@gmail.com>
2024-03-28 22:20:22 -0400

    clear fix: 15 characters now, not 8

--- src/patches/clearFocusedNamesFromFix.asm
+++ src/patches/clearFocusedNamesFromFix.asm
@@ -2,7 +2,7 @@ move.w #$20, $3c0004 ; VRAMMOD=32, skip an entire column of fix after each write
 
 ;; player one side
 move.w #$P1_FOCUSED_NAME_FIX_ADDRESS_VALUE, $3c0000  ; starting fix address TO VRAMADDR
-move.w #7, D4 ; clear out 8 tiles, one less due to dbra hinging on -1
+move.w #14, D4 ; clear out 15 tiles, one less due to dbra hinging on -1
 
 p1_clearFixTile:
 move.w #$f20, $3C0002 ; set to tile the blank tile
@@ -10,7 +10,7 @@ dbra D4, p1_clearFixTile
 
 ;; player two side
 move.w #$P2_FOCUSED_NAME_FIX_ADDRESS_VALUE, $3c0000  ; starting fix address TO VRAMADDR
-move.w #7, D4 ; clear out 8 tiles, one less due to dbra hinging on -1
+move.w #14, D4 ; clear out 15 tiles, one less due to dbra hinging on -1
 
 p2_clearFixTile:
 move.w #$f20, $3C0002 ; set to tile the blank tile
