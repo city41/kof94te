75a3f67d1bde34647d186d47a495f7563629fce0
Matt Greer <matt.e.greer@gmail.com>
2024-04-03 11:07:28 -0400

    loadPalettes: 4 at a time for speed increase

---
+++ scripts/toneosd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf toneosd
+mkdir toneosd
+cd toneosd
+cp $MAME_ROM_DIR/kof94.zip .
+unzip kof94.zip
+neosdconv -i . -o ../kof94te.neo -y 2024 -n "The King of Fighters '94:TE hack" -g Fighting -m SNK_city41
