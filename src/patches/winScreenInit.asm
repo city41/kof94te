da4b180f1c9c36668940983479978253e4441f4f
Matt Greer <matt.e.greer@gmail.com>
2024-04-09 23:42:59 -0400

    winScreenInit: show p1 or p2 winning chars

--- src/patches/winScreenInit.asm
+++ src/patches/winScreenInit.asm
@@ -24,6 +24,26 @@ jsr $3fd58
 bra done
 
 humanWon:
+
+;; first, who won? p1 or p2? need to consider versus mode
+btst #0, $NUM_PLAYER_MODE ; is p1 playing?
+beq onlyPlayerTwoIsHuman
+cmpi.b #$80, $108238 ; player 1 is human, did they lose?
+beq playerOneLost
+lea $P1_CHOSEN_CHAR0, A6 ; player 1 won, get their characters ready
+bra setupChars
+
+playerOneLost:
+; this branches here too because if only p2 is playing, they beat the cpu
+; if the cpu won, we would have bailed up above
+onlyPlayerTwoIsHuman:
+lea $P2_CHOSEN_CHAR2, A6 ; player 2 won, get their characters ready
+
+
+setupChars:
+
+movem.l A4, $MOVEM_STORAGE
+
 ;;;just always pick the team leader routine for now
 ;;; there are three win screen init routines, each put characters in different locations
 ;;; depending on who won the match, D2 will be 0, 1 or 2
@@ -31,11 +51,15 @@ humanWon:
 move.w #0, D2 ; here we are just forcing the game to always choose the first routine
 
 lea $AFTER_SCREEN_POSITION_TABLE, A0 ; get our position table set up
+;; this winning team list is needed due to PX_CHOSEN_CHARS being word based
+;; the game wants a byte based list
+lea $WINNING_TEAM_LIST, A4 ; get the winning list ready to be populated
 
 ;; first character, move to center
 clr.w D6
 lea $62756, A2
-move.b $P1_CHOSEN_CHAR0, D6
+move.b (A6), D6
+move.b D6, (A4) ; stick it into the winning list
 add.w D6, D6
 add.w D6, D6
 move.w (A2, D6.w), D7
@@ -44,7 +68,8 @@ move.w D7, (A0, D6.w)
 ;; second character, to a side
 clr.w D6
 lea $626ee, A2
-move.b $P1_CHOSEN_CHAR1, D6
+move.b $2(A6), D6
+move.b D6, $1(A4) ; stick it into the winning list
 add.w D6, D6
 add.w D6, D6
 move.w (A2, D6.w), D7
@@ -53,13 +78,16 @@ move.w D7, (A0, D6.w)
 ;; third character, to a side
 clr.w D6
 lea $627be, A2
-move.b $P1_CHOSEN_CHAR2, D6
+move.b $4(A6), D6
+move.b D6, $2(A4) ; stick it into the winning list
 add.w D6, D6
 add.w D6, D6
 move.w (A2, D6.w), D7
 move.w D7, (A0, D6.w)
 
-lea $P1_CHOSEN_CHAR0, A2 ; point the game at our custom team list instead of the canned ones
+movea.l A4, A2 ; point the game at the winning team list instead of the canned ones
+
+movem.l $MOVEM_STORAGE, A4
 
 done:
 rts
