ecd064e4f5f73d4eeec007a66b04e7c392f6680c
Matt Greer <matt.e.greer@gmail.com>
2024-04-11 13:47:05 -0400

    winScreenInit: fudge teams as needed, fixes #35

--- src/patches/winScreenInit.asm
+++ src/patches/winScreenInit.asm
@@ -4,6 +4,8 @@
 
 ;;; nothing to restore from the clobber
 
+movem.l A4/A5, $MOVEM_STORAGE
+
 ;; did the cpu win? if so, bail and just let the original win scene routines run
 btst #0, $NUM_PLAYER_MODE ; is p1 playing?
 beq checkPlayerTwo
@@ -21,12 +23,44 @@ bne humanWon
 
 ;; cpu won, so just run the original routines
 cpuWon:
+;; but first, set human's team to not be a mirror match or england
+btst #0, $NUM_PLAYER_MODE ; is p1 playing?
+beq cpuWon_setPlayerTwoTeam
+lea $108231, A4
+lea $108431, A5
+bsr fudgeTeams
+bra cpuWon_done
+
+cpuWon_setPlayerTwoTeam:
+lea $108431, A4
+lea $108231, A5
+bsr fudgeTeams
+bra cpuWon_done
+
+cpuWon_done:
 jsr $3fd58
 bra done
 
 humanWon:
+btst #0, $NUM_PLAYER_MODE ; is p1 playing?
+beq alteringTeams_checkP2
+;; p1 is a human, make sure to fix the teams if needed
+lea $108231, A4
+lea $108431, A5
+bsr fudgeTeams
+bra doneAlteringTeams
 
-;; first, who won? p1 or p2? need to consider versus mode
+alteringTeams_checkP2:
+btst #1, $NUM_PLAYER_MODE ; is p2 playing?
+beq doneAlteringTeams
+;; p2 is a human, make sure to fix the teams if needed
+lea $108431, A4
+lea $108231, A5
+bsr fudgeTeams
+
+
+doneAlteringTeams:
+;; second, who won? p1 or p2? need to consider versus mode
 btst #0, $NUM_PLAYER_MODE ; is p1 playing?
 beq onlyPlayerTwoIsHuman
 cmpi.b #$80, $108238 ; player 1 is human, did they lose?
@@ -44,8 +78,6 @@ lea $P2_CHOSEN_CHAR2, A6 ; player 2 won, get their characters ready
 setupChars:
 
 move.b #0, $WIN_SCREEN_ALREADY_SET_LEFT
-
-movem.l A4, $MOVEM_STORAGE
 
 ;;; there are three win screen init routines, each put characters in different locations
 ;;; depending on who won the match, D2 will be 0, 1 or 2
@@ -101,7 +133,30 @@ dbra D5, loadCharactersLoop
 
 movea.l A4, A2 ; point the game at the winning team list instead of the canned ones
 
-movem.l $MOVEM_STORAGE, A4
-
 done:
+movem.l $MOVEM_STORAGE, A4/A5
+rts
+
+;;; fudgeTeams
+; a routine that will make sure
+; - the team in question is not england
+; - the teams are not a "mirror match"
+; - it will adjust the team ids in memory as needed
+; parameters
+; -- A4: the first team's id address (either 108231 or 108431)
+; -- A5: the opposite team's id address
+fudgeTeams:
+cmpi.b #$7, (A4)
+bne fudgeTeams_p1doneWithEnglandCheck
+move.b #$5, (A4) ; they were England, so make them Italy
+fudgeTeams_p1doneWithEnglandCheck:
+move.b (A4), D2 ; load current player's team
+cmp.b (A5), D2  ; is this a "mirror match"?
+bne fudgeTeams_done ; not a mirror match? we're good then
+;; uh oh this is a mirror match
+addi.b #1, (A4) ; increment current team to the next one
+cmpi.b #6, (A4)
+ble fudgeTeams_done
+move.b #0, (A4) ; either team id got out of range or became England, either case, set it to Brazil
+fudgeTeams_done:
 rts
