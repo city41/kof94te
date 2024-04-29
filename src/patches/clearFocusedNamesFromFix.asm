eb9f1431f9c8fe03c1dc07bd67395ec8313cfa29
Matt Greer <matt.e.greer@gmail.com>
2024-04-29 18:52:40 -0400

    clarFocusednamesFromFix: clear all 18 fix chars

--- src/patches/clearFocusedNamesFromFix.asm
+++ src/patches/clearFocusedNamesFromFix.asm
@@ -2,7 +2,7 @@ move.w #$20, $3c0004 ; VRAMMOD=32, skip an entire column of fix after each write
 
 ;; player one side
 move.w #$P1_FOCUSED_NAME_FIX_ADDRESS_VALUE, $3c0000  ; starting fix address TO VRAMADDR
-move.w #14, D4 ; clear out 15 tiles, one less due to dbra hinging on -1
+move.w #17, D4 ; clear out 18 tiles, one less due to dbra hinging on -1
 
 p1_clearFixTile:
 move.w #$f20, $3C0002 ; set to tile the blank tile
