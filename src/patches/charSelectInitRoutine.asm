f9131d08100b5fe85dc8cfc7fa39dd7ed1095dcb
Matt Greer <matt.e.greer@gmail.com>
2024-05-26 21:38:32 -0400

    load alllll the cpu cursors

--- src/patches/charSelectInitRoutine.asm
+++ src/patches/charSelectInitRoutine.asm
@@ -62,16 +62,40 @@ move.w #0, $P1_CURSOR_Y
 ;;;;;;;;;;;;;;; INIT CPU AGAINST P1 ;;;;;;;;;;;;;;
 cmpi.b #1, $BIOS_PLAYER_MOD2 ;; is player 2 playing?
 beq skipPlayer1 ; player 2 is playing, this is versus mode, so don't do cpu
-; load the cpu cursor, left side
-; it loads itself off screen, no need to move it
+
+; load the cpu cursors
+; cpu cursor 1, left side
 move.w #$P2_CPU_CURSOR_CHAR1_LEFT_SI, D6
 lea $2P2_CPU_CURSOR_LEFT_WHITE_IMAGE, A6
 move.w #0, D5              ; offset into tile data
 jsr $2RENDER_STATIC_IMAGE
 
-; load the cpu cursor, right side
-; it loads itself off screen, no need to move it
+; cpu cursor 1, right side
 move.w #$P2_CPU_CURSOR_CHAR1_RIGHT_SI, D6
+lea $2P2_CPU_CURSOR_RIGHT_WHITE_IMAGE, A6
+move.w #0, D5              ; offset into tile data
+jsr $2RENDER_STATIC_IMAGE
+
+; cpu cursor 2, left side
+move.w #$P2_CPU_CURSOR_CHAR2_LEFT_SI, D6
+lea $2P2_CPU_CURSOR_LEFT_WHITE_IMAGE, A6
+move.w #0, D5              ; offset into tile data
+jsr $2RENDER_STATIC_IMAGE
+
+; cpu cursor 2, right side
+move.w #$P2_CPU_CURSOR_CHAR2_RIGHT_SI, D6
+lea $2P2_CPU_CURSOR_RIGHT_WHITE_IMAGE, A6
+move.w #0, D5              ; offset into tile data
+jsr $2RENDER_STATIC_IMAGE
+
+; cpu cursor 3, left side
+move.w #$P2_CPU_CURSOR_CHAR3_LEFT_SI, D6
+lea $2P2_CPU_CURSOR_LEFT_WHITE_IMAGE, A6
+move.w #0, D5              ; offset into tile data
+jsr $2RENDER_STATIC_IMAGE
+
+; cpu cursor 3, right side
+move.w #$P2_CPU_CURSOR_CHAR3_RIGHT_SI, D6
 lea $2P2_CPU_CURSOR_RIGHT_WHITE_IMAGE, A6
 move.w #0, D5              ; offset into tile data
 jsr $2RENDER_STATIC_IMAGE
@@ -95,16 +119,40 @@ move.w #0, $P2_CURSOR_Y
 move.b $BIOS_PLAYER_MOD1, D6 ; are they even playing?
 cmpi.b #1, D6 ;; look specifically for 1: playing
 beq skipPlayer2 ; player 1 is playing, this is versus mode, so don't do cpu
-; load the cpu cursor, left side
-; it loads itself off screen, no need to move it
+
+; load the cpu cursors
+; cpu cursor 1, left side
 move.w #$P1_CPU_CURSOR_CHAR1_LEFT_SI, D6
 lea $2P1_CPU_CURSOR_LEFT_WHITE_IMAGE, A6
 move.w #0, D5              ; offset into tile data
 jsr $2RENDER_STATIC_IMAGE
 
-; load the cpu cursor, right side
-; it loads itself off screen, no need to move it
+; cpu cursor 1, right side
 move.w #$P1_CPU_CURSOR_CHAR1_RIGHT_SI, D6
+lea $2P1_CPU_CURSOR_RIGHT_WHITE_IMAGE, A6
+move.w #0, D5              ; offset into tile data
+jsr $2RENDER_STATIC_IMAGE
+
+; cpu cursor 2, left side
+move.w #$P1_CPU_CURSOR_CHAR2_LEFT_SI, D6
+lea $2P1_CPU_CURSOR_LEFT_WHITE_IMAGE, A6
+move.w #0, D5              ; offset into tile data
+jsr $2RENDER_STATIC_IMAGE
+
+; cpu cursor 2, right side
+move.w #$P1_CPU_CURSOR_CHAR2_RIGHT_SI, D6
+lea $2P1_CPU_CURSOR_RIGHT_WHITE_IMAGE, A6
+move.w #0, D5              ; offset into tile data
+jsr $2RENDER_STATIC_IMAGE
+
+; cpu cursor 3, left side
+move.w #$P1_CPU_CURSOR_CHAR3_LEFT_SI, D6
+lea $2P1_CPU_CURSOR_LEFT_WHITE_IMAGE, A6
+move.w #0, D5              ; offset into tile data
+jsr $2RENDER_STATIC_IMAGE
+
+; cpu cursor 3, right side
+move.w #$P1_CPU_CURSOR_CHAR3_RIGHT_SI, D6
 lea $2P1_CPU_CURSOR_RIGHT_WHITE_IMAGE, A6
 move.w #0, D5              ; offset into tile data
 jsr $2RENDER_STATIC_IMAGE
@@ -118,32 +166,79 @@ cmpi.b #0, $PLAY_MODE
 bne skipDemoMode
 ;; this is demo mode, we need to do both cpu cursors
 
-;; p1 side cpu cursor, left half
+;; p1 side cpu cursors
+; cpu cursor 1, left side
 move.w #$P1_CPU_CURSOR_CHAR1_LEFT_SI, D6
 lea $2P1_CPU_CURSOR_LEFT_WHITE_IMAGE, A6
 move.w #0, D5              ; offset into tile data
 jsr $2RENDER_STATIC_IMAGE
 
-; p1 side cpu cursor, right half
-; it loads itself off screen, no need to move it
+; cpu cursor 1, right side
 move.w #$P1_CPU_CURSOR_CHAR1_RIGHT_SI, D6
 lea $2P1_CPU_CURSOR_RIGHT_WHITE_IMAGE, A6
 move.w #0, D5              ; offset into tile data
 jsr $2RENDER_STATIC_IMAGE
 
-;; p1 side cpu cursor, left half
+; cpu cursor 2, left side
+move.w #$P1_CPU_CURSOR_CHAR2_LEFT_SI, D6
+lea $2P1_CPU_CURSOR_LEFT_WHITE_IMAGE, A6
+move.w #0, D5              ; offset into tile data
+jsr $2RENDER_STATIC_IMAGE
+
+; cpu cursor 2, right side
+move.w #$P1_CPU_CURSOR_CHAR2_RIGHT_SI, D6
+lea $2P1_CPU_CURSOR_RIGHT_WHITE_IMAGE, A6
+move.w #0, D5              ; offset into tile data
+jsr $2RENDER_STATIC_IMAGE
+
+; cpu cursor 3, left side
+move.w #$P1_CPU_CURSOR_CHAR3_LEFT_SI, D6
+lea $2P1_CPU_CURSOR_LEFT_WHITE_IMAGE, A6
+move.w #0, D5              ; offset into tile data
+jsr $2RENDER_STATIC_IMAGE
+
+; cpu cursor 3, right side
+move.w #$P1_CPU_CURSOR_CHAR3_RIGHT_SI, D6
+lea $2P1_CPU_CURSOR_RIGHT_WHITE_IMAGE, A6
+move.w #0, D5              ; offset into tile data
+jsr $2RENDER_STATIC_IMAGE
+
+; p2 side cpu cursors
+; cpu cursor 1, left side
 move.w #$P2_CPU_CURSOR_CHAR1_LEFT_SI, D6
 lea $2P2_CPU_CURSOR_LEFT_WHITE_IMAGE, A6
 move.w #0, D5              ; offset into tile data
 jsr $2RENDER_STATIC_IMAGE
 
-; p1 side cpu cursor, right half
-; it loads itself off screen, no need to move it
+; cpu cursor 1, right side
 move.w #$P2_CPU_CURSOR_CHAR1_RIGHT_SI, D6
 lea $2P2_CPU_CURSOR_RIGHT_WHITE_IMAGE, A6
 move.w #0, D5              ; offset into tile data
 jsr $2RENDER_STATIC_IMAGE
 
+; cpu cursor 2, left side
+move.w #$P2_CPU_CURSOR_CHAR2_LEFT_SI, D6
+lea $2P2_CPU_CURSOR_LEFT_WHITE_IMAGE, A6
+move.w #0, D5              ; offset into tile data
+jsr $2RENDER_STATIC_IMAGE
+
+; cpu cursor 2, right side
+move.w #$P2_CPU_CURSOR_CHAR2_RIGHT_SI, D6
+lea $2P2_CPU_CURSOR_RIGHT_WHITE_IMAGE, A6
+move.w #0, D5              ; offset into tile data
+jsr $2RENDER_STATIC_IMAGE
+
+; cpu cursor 3, left side
+move.w #$P2_CPU_CURSOR_CHAR3_LEFT_SI, D6
+lea $2P2_CPU_CURSOR_LEFT_WHITE_IMAGE, A6
+move.w #0, D5              ; offset into tile data
+jsr $2RENDER_STATIC_IMAGE
+
+; cpu cursor 3, right side
+move.w #$P2_CPU_CURSOR_CHAR3_RIGHT_SI, D6
+lea $2P2_CPU_CURSOR_RIGHT_WHITE_IMAGE, A6
+move.w #0, D5              ; offset into tile data
+jsr $2RENDER_STATIC_IMAGE
 ;;;;;;;;;;;;;;;;; END DEMO MODE INIT ;;;;;;;;;;;;;;;;;
 
 skipDemoMode:
