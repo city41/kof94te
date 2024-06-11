c1665370b95f43f75152c63e6ccf549507523f6f
Matt Greer <matt.e.greer@gmail.com>
2024-06-11 08:32:16 -0400

    guardPalettes.asm

---
+++ src/patches/guardPalettes.asm
@@ -0,0 +1,19 @@
+;; guardPalettes
+;; while in character select, makes sure the game doesn't clobber our palettes
+;;
+;; but the game needs it palettes too, so instead of clobber they are written
+;; to the side, and restored later during teardown
+
+cmpi.b #1, $IN_CHAR_SELECT_FLAG
+bne skip ; not in char select? nothing to do
+cmpi.w #15, D0
+ble skip ; not touching one of our palettes? nothing to do
+cmpi.w #140, D0
+bge skip ; not touching one of our palettes? nothing to do
+move.w #140, D0  ; have it write into a palette that isn't ours
+
+skip:
+lea $400000, A1
+
+done:
+rts
