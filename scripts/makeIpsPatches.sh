065326b3bcab376fb062caf383708e17d0db6d42
Matt Greer <matt.e.greer@gmail.com>
2024-06-12 10:47:10 -0400

    makeIpsPatches: builds both variants

--- scripts/makeIpsPatches.sh
+++ scripts/makeIpsPatches.sh
@@ -1,40 +1,52 @@
-#!/bin/bash
+#!/bin/bash -x
 
 GAME='kof94'
 
 rm -rf ipsBinaries
 rm -rf ipsPatches
 
-mkdir ipsPatches
 mkdir ipsBinaries
 mkdir ipsBinaries/original
-mkdir ipsBinaries/hacked
+mkdir ipsPatches
 
 yarn restore;
-cp $MAME_ROM_DIR/$GAME.zip ipsBinaries/original/
+cp $MAME_ROM_DIR/${GAME}.zip ipsBinaries/original/
+(cd ipsBinaries/original/ && unzip ${GAME}.zip)
 
-yarn ts-node src/patchRom/main.ts src/patches/kof94te.json
-cp $MAME_ROM_DIR/$GAME.zip ipsBinaries/hacked/
+buildPatch() {
+    variant=$1
 
-(cd ipsBinaries/original/ && unzip $GAME.zip)
-(cd ipsBinaries/hacked/ && unzip $GAME.zip)
+    echo "building variant: ${variant}"
 
-for f in `ls ipsBinaries/original/ -I kof94.zip`; do
-    bf=`basename $f`
+    mkdir ipsPatches/${variant}
+    mkdir ipsBinaries/hacked_${variant}
 
-    originalFile="ipsBinaries/original/$bf"
-    hackedFile="ipsBinaries/hacked/$bf"
+    yarn run-srom-crom-${variant}
+    yarn ts-node src/patchRom/main.ts src/patches/kof94te_${variant}.json
+    cp $MAME_ROM_DIR/$GAME.zip ipsBinaries/hacked_${variant}
 
-    originalSha=$(sha256sum "$originalFile" | awk '{ print $1 }' )
-    hackedSha=$(sha256sum "$hackedFile" | awk '{ print $1 }' )
+    (cd ipsBinaries/hacked_${variant}/ && unzip ${GAME}.zip)
 
-    echo "$bf originalSha: $originalSha"
-    echo "$bf hackedSha: $hackedSha"
+    for f in `ls ipsBinaries/original/ -I kof94.zip`; do
+        bf=`basename $f`
 
-    if [ "$originalSha" != "$hackedSha" ]; then
-        echo "Creating ips for $bf"
-        yarn ts-node src/tools/makeIpsPatch.ts ipsBinaries/original/$bf ipsBinaries/hacked/$bf ipsPatches/$GAME.$bf.ips
-    fi
-done
+        originalFile="ipsBinaries/original/${bf}"
+        hackedFile="ipsBinaries/hacked_$variant/${bf}"
 
-(cd ipsPatches && zip kof94teIpsPatches.zip *.ips)
+        originalSha=$(sha256sum "${originalFile}" | awk '{ print $1 }' )
+        hackedSha=$(sha256sum "${hackedFile}" | awk '{ print $1 }' )
+
+        echo "${bf} originalSha: ${originalSha}"
+        echo "${bf} hackedSha: ${hackedSha}"
+
+        if [ "${originalSha}" != "${hackedSha}" ]; then
+            echo "Creating ${variant} ips for ${bf}"
+            yarn ts-node src/tools/makeIpsPatch.ts ipsBinaries/original/${bf} ipsBinaries/hacked_${variant}/${bf} ipsPatches/${variant}/${GAME}_${variant}.${bf}.ips
+        fi
+    done
+
+    (cd ipsPatches/${variant} && zip kof94teIpsPatches_${variant}.zip *.ips)
+}
+
+buildPatch a94
+buildPatch a95
