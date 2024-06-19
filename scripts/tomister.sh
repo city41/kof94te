120c74c7a4faf8da0593544c93668586c0cc156a
Matt Greer <matt.e.greer@gmail.com>
2024-06-19 19:34:09 -0400

    wip

---
+++ scripts/tomister.sh
@@ -0,0 +1,1 @@
+scp kof94te.neo root@192.168.1.$1:/media/fat/games/NEOGEO
--- scripts/toneosd.sh
+++ scripts/toneosd.sh
@@ -5,4 +5,4 @@ mkdir toneosd
 cd toneosd
 cp $MAME_ROM_DIR/kof94.zip .
 unzip kof94.zip
-neosdconv -i . -o ../kof94te.neo -y 2024 -n "The King of Fighters '94:TE hack" -g Fighting -m SNK_city41 -# 729
+neosdconv -i . -o ../kof94te.neo -y 2024 -n "The King of Fighters '94:TE hack" -g Fighting -m SNK_city41 -# 55
