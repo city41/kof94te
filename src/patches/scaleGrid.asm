0969577e703ae1eacf47aad68a60bd61aea8ee62
Matt Greer <matt.e.greer@gmail.com>
2024-06-23 20:41:17 -0400

    y position going down implemented, but not working

--- src/patches/scaleGrid.asm
+++ src/patches/scaleGrid.asm
@@ -1,8 +1,23 @@
-move.w #$8000 + $GRID_IMAGE_SI, D2
-add.w D1, D2
-move.w D2, $3c0000
-move.w #$f00, D0
-add.w $GRID_SCALE_COUNTDOWN, D0
-move.w D0, $3c0002
+;; scaleGrid
+;; when character select is done, this method scales the grid down as a nice visual
+;; effect before moving onto order select
+;;
+;; character_grid is sticky, so operations are only needed on first sprite
+
+;; first read its current SCB3 value to get the vertical position
+move.w #$8000 + $GRID_IMAGE_SI, $3c0000 ; set up the proper vram address
+
+move.w $3c0002, D1 ; pull scb3 out
+lsr.w #7, D1       ; shift D1 down to get at y position
+addi.w #1, D1      ; shift y down
+lsl.w #7, D1       ; shift y position back to where vram wants it
+
+move.w #$f00, D0   ; set horizontal scale to full
+add.w $GRID_SCALE_COUNTDOWN, D0 ; add in current vertical scale
+add.w D0, D1       ; pack it all up into a single scb3 word
+
+move.w D1, $3c0002 ; and send it on back
+
+
 
 rts
